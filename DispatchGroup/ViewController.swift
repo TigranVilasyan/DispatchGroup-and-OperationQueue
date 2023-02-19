15342b731a7752c35fb8a275e5cb8871bbb13954
TigranVilasyan <tvilasyan@gmail.com>
2023-02-19 16:02:10 +0400

    minor changes
    minor changes

--- DispatchGroup/ViewController.swift
+++ DispatchGroup/ViewController.swift
@@ -59,7 +59,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        dispatchGroupCall()
+        //        dispatchGroupCall()
         operationQueue()
     }
     
